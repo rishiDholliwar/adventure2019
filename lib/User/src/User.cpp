ac517d2486852e9f6c7e6f75e9a63f32ad67904c
lkoo <lkoo@cs-sc4050u-d03.cs.surrey.sfu.ca>
2019-01-17 18:39:40 -0800

    added getters and setters and some abstraction to User.h and User.cpp (lkoo)

--- lib/User/src/User.cpp
+++ lib/User/src/User.cpp
@@ -4,6 +4,26 @@
 
 #include "../include/User.h"
 
+void setConnection(const long connectionId) {
+	this -> _connectionId = connectionId;
+}
+
+long getConnectionId() {
+	return _connectionId;
+}
+
+void clearConnection() {
+	setConnection(0);
+}
+
+bool isUsernameEqual(std::string userName) {
+	return _userName == userName;
+}
+
+bool isHashedPasswordEqual(std::string hashedPassword) {
+	return _hashedPassword == hashedPassword;
+}
+
 void User::moveToRoom(int roomNumber) {
     this->_roomNumber = roomNumber;
 }
