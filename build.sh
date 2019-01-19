a447b117b64fa2b3400dc8ccb4b2b3bb9dd2b90d
elvinw <elvinw@sfu.ca>
2019-01-18 21:10:43 -0800

    Added Enter option to kill chatserver
    Added build dir to .gitignore

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ cd networkbuild;
 cmake ..;
 make;
 lsof -ti:4000 | xargs kill -9 > /dev/null 2>&1;
-bin/chatserver 4000 ../webchat.html;
-
-
+(bin/chatserver 4000 ../webchat.html;) &
+echo "Hit Enter to kill server"
+read enter
+killall chatserver
