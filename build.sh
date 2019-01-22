f303ea291a79d1d4eac0ccc9e38c5f40dc7b4289
elvinw <elvinw@sfu.ca>
2019-01-21 23:40:26 -0800

    Added some quality of life features to build script

--- build.sh
+++ build.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
-mkdir -p networkbuild;
-cd networkbuild;
+PORT=4000
+JOBS=0
+for var in "$@"
+do
+	IFS='=' read -r -a array <<< "$var"
+	declare "${array[0]}"="${array[1]}"
+done
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+mkdir -p $DIR/networkbuild;
+cd $DIR/networkbuild;
 cmake ..;
-make;
-lsof -ti:4000 | xargs kill -9 > /dev/null 2>&1;
-(bin/chatserver 4000 ../webchat.html;) &
+if [ -z $JOBS ]
+then
+	make;
+else
+	make -j$JOBS;
+fi
+lsof -ti:$PORT | xargs kill -9 > /dev/null 2>&1;
+(bin/chatserver $PORT ../webchat.html;) &
+echo "Running server on port: $PORT"
 echo "Hit Enter to kill server"
 read enter
 killall chatserver
