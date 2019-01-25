6688e52a9661507376ac63c6cbd81a8a64b68ffe
bshetty <bshetty@sfu.ca>
2019-01-25 14:42:42 -0800

    Cleaned up characterController & worldmanager

--- lib/WorldManager/src/WorldManager.cpp
+++ lib/WorldManager/src/WorldManager.cpp
@@ -1,5 +1,4 @@
 #include <utility>
-#include "../include/WorldManager.h"
 #include <iostream>
 #include <WorldManager.h>
 
@@ -30,15 +29,13 @@ std::string WorldManager::move(std::string userName, std::string input) {
     // Verify if direction is valid
     if(!roomController.isDirectionValid(character.getRoomID(), input)){
         return "That isnt a valid direction";
-    }else {
-        unsigned int destinationRoomID = roomController.getRoomID(character.getRoomID, input);
-        charController.updateRoomID(character, destinationRoomID);
-
-        // Update roomList to account for character moving
-        roomController.updateCharacterList(userName, character.getRoomID(), destinationRoomID);
-        return userName + " has moved " + input;
     }
+    unsigned int destinationRoomID = roomController.getRoomID(character.getRoomID, input);
+    charController.updateRoomID(character, destinationRoomID);
 
+    // Update roomList to account for character moving
+    roomController.updateCharacterList(userName, character.getRoomID(), destinationRoomID);
+    return userName + " has moved " + input;
 }
 
 std::string WorldManager::pickUp(std::string userName, std::string input) {
@@ -53,16 +50,14 @@ std::string WorldManager::pickUp(std::string userName, std::string input) {
     // Verify if item exists in the room
     if(!roomController.doesItemExist(character.getRoomID(), item.getItemID())) {
         return "Item: " + input + " doesnt exist in this room!";
-    } else {
-        if(!charController.addItemToInventory(character, item)){
-            return "You have no room for this item";
-        }else{
-            // Update room to no longer have the item (until the next reset)
-            roomController.removeItem(character.getRoomID, input);
+    } 
 
-            return "Item added to inventory!";
-        }
+    if(!charController.addItemToInventory(character, item)){
+        return "You have no room for this item";
     }
+    // Update room to no longer have the item (until the next reset)
+    roomController.removeItem(character.getRoomID, input);
+    return "Item added to inventory!";
 }
 
 std::string WorldManager::drop(std::string userName, std::string input) {
@@ -77,15 +72,15 @@ std::string WorldManager::drop(std::string userName, std::string input) {
     // Verify if the character has item
     if(!character.hasItem(item.getItemID())){
         return "You don't have this item in your inventory";
-    }else{
-        // Remove item from character's inventory
-        character.dropItem(item.getItemID());
-
-        // Add item to room's item List
-        roomController.addItem(character.getRoomID(), item);
-
-        return "You dropped " + input + " into the room!";
     }
+
+    // Remove item from character's inventory
+    character.dropItem(item.getItemID());
+
+    // Add item to room's item List
+    roomController.addItem(character.getRoomID(), item);
+
+    return "You dropped " + input + " into the room!";  
 }
 
 std::string WorldManager::consume(std::string userName, std::string input) {
@@ -100,20 +95,20 @@ std::string WorldManager::consume(std::string userName, std::string input) {
     // Verify if character has the item
     if(!character.hasItem(item.getItemID())){
         return "You don't have this item in your inventory";
-    }else{
-        // Check if item is consumable
-        if(!character.isConsumable(item)){
-            return "The item is not a consumable!";
-        }else {
-            // Consume Item and gain it's effects
-            character.consume(item.getItemID());
-
-            // Remove item from character's inventory
-            character.dropItem(item.getItemID());
-
-            return "you used " + input + " !";
-        }
     }
+
+    // Check if item is consumable
+    if(!character.isConsumable(item)){
+        return "The item is not a consumable!";
+    }
+    // Consume Item and gain it's effects
+    character.consume(item.getItemID());
+
+    // Remove item from character's inventory
+    character.dropItem(item.getItemID());
+
+    return "you used " + input + " !";
+      
 }
 
 
