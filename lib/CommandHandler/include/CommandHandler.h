1de164e3069920d982d7a4f94a15838cb733f5c8
elvinw <elvinw@sfu.ca>
2019-02-08 18:48:00 -0800

    Added comments to CommandHandler

--- lib/CommandHandler/include/CommandHandler.h
+++ lib/CommandHandler/include/CommandHandler.h
@@ -1,7 +1,6 @@
 #ifndef WEBSOCKETNETWORKING_COMMANDHANDLER_H
 #define WEBSOCKETNETWORKING_COMMANDHANDLER_H
 
-
 #include <deque>
 #include <vector>
 #include <unordered_map>
@@ -22,7 +21,13 @@ enum CommandType
     COMMANDHANDLER,
     UNKNOWN
 };
-
+/*
+     * Command Handler
+     *
+     * Stores and maps our commands to a string
+     *
+     * Command function pointers are returned to be used when found in our maps
+*/
 class CommandHandler {
 public:
     using Name = std::string;
@@ -37,18 +42,89 @@ public:
     using LognFunctionMap = std::unordered_map<Command, LognCommFunc>;
 
     CommandHandler() = default;
+
+    /*
+     * Retrieves the User Function pointer to a gamecontroller function
+     *
+     * Pre-Condition: Takes in a Name(std::string) and Command(std::string)
+     *
+     * Post-Condition: Returns a member function pointer
+     *                  or nullptr if fails to find command
+    */
     UserCommFunc getUserFunc(const Name& userName, const Command& command);
+
+    /*
+     * Retrieves the Command function pointer to a Command Handler function
+     *
+     * Pre-Condition: Takes in a Command(std::string)
+     *
+     * Post-Condition: Returns a member function pointer
+     *                  or nullptr if fails to find command
+    */
     CommHandFunc getCommFunc(const Command& command);
+
+    /*
+     * Retrieves the login function pointer to a User Controller function
+     *
+     * Pre-Condition: Takes in a Command(std::string)
+     *
+     * Post-Condition: Returns a member function pointer
+     *                  or nullptr if fails to find command
+    */
     LognCommFunc getLognFunc(const Command& command);
 
+    /*
+     * Parses the arguments for _setAlias
+     * Calls _setAlias is args are valid
+     *
+     * Pre-Condition: Takes in a Name(std::string) and input
+     *
+     * Post-Condition: Returns an output string
+    */
     std::string setAlias(const Name& userName, const std::string& input);
+
+    /*
+     * Parses the arguments for _removeAlias
+     * Calls _removeAlias is args are valid
+     *
+     * Pre-Condition: Takes in a Name(std::string) and input
+     *
+     * Post-Condition: Returns an output string
+    */
     std::string removeAlias(const Name& userName, const std::string& input);
+
+    /*
+     * Sets user alias for a command (Only works for game controller commands)
+     *
+     * Pre-Condition: Takes in a Name(std::string) and input
+     *
+     * Post-Condition: Returns an output string
+    */
     std::string _setAlias(const Name& userName, const Command& command, const Alias& alias);
+
+    /*
+     * removes user alias for a command (Only works for game controller commands)
+     *
+     * Pre-Condition: Takes in a Name(std::string) and input
+     *
+     * Post-Condition: Returns an output string
+    */
     std::string _removeAlias(const Name& userName, const Alias& alias);
 
+    /*
+     * Parses input and determines the type of command it is
+     *
+     * Pre-Condition: Takes in input
+     *
+     * Post-Condition: Returns a struct with CommandType, Command(std::string),
+     *                  and the rest of the command(input)
+    */
     CommandInfo parseCommand(const std::string& input);
 
 private:
+    /*
+     * Maps to function pointers
+    */
     static UserFunctionMap _defUserMap;
     static CommFunctionMap _defCommMap;
     static LognFunctionMap _defLognMap;
